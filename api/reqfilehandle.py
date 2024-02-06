241fe71c120a108762adda05efc033f4a73fbc51
phichayut N <snokitsune@gmail.com>
2024-02-06 11:37:34 +0700

    pathfix

--- api/reqfilehandle.py
+++ api/reqfilehandle.py
@@ -1,17 +1,16 @@
-from pathlib import Path
+from pathlib import PurePath
 import requests
-
-path_clean = lambda x: x if not x.endswith("/") else x[:-1]
+import os
 
 class RequestFileHandler():
     def __init__(self, MISP_API: str, MISP_API_TOKEN: str, RULES_PATH: str, RULES_TEMP_PATH: str):
         self.api = f"http://{MISP_API}/events/nids/suricata/download"
         self.token = MISP_API_TOKEN
-        self.storage_path = path_clean(RULES_PATH)
-        self.temp_storage_path = path_clean(RULES_TEMP_PATH)
+        self.storage_path = os.path.join(PurePath(RULES_PATH), "suricata-misp.rules")
+        self.temp_storage_path = os.path.join(PurePath(RULES_PATH), "suricata-misp.tmp.rules")
 
-        self.file = open(f"{RULES_PATH}/suricata-misp.rules", "w+", encoding="utf-8")
-        self.temp = open(f"{RULES_TEMP_PATH}/suricata-misp.rules", "w+", encoding="utf-8")
+        self.file = open(self.storage_path, "w+", encoding="utf-8")
+        self.temp = open(self.temp_storage_path, "w+", encoding="utf-8")
 
     def getParams(self) -> dict:
         return {k:v for k,v in self.__dict__.items()}
--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import uvicorn
 
 # runs app.py with uvicorn server
 if __name__ == "__main__":
-    uvicorn.run("api.app:app", host="0.0.0.0", port=80, reload=True)
+    uvicorn.run("api.app:app", host="0.0.0.0", port=8080, reload=True)
