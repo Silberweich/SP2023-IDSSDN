efc9cc61634a8f948df8f7bf92fa58fed885a92b
phichayut N <snokitsune@gmail.com>
2024-02-15 15:14:04 +0700

    update ryu rule hash checker

--- ryulehash.py
+++ ryulehash.py
@@ -5,16 +5,37 @@ import syslog
 import dotenv
 import os
 
+HASH_LOC = "/etc/ryuhash/rules.hash"
+HASH_DIR = "/etc/ryuhash/"
+
 ## Use with cron "*/1 * * * * /path/to/ryuhash.py"
+## .env format
+# MISP_API =
+# MISP_API_TOKEN = 
+# SURICATA-ADDRESSES = 192.168.0.1:192.168.0.2:...
+
+# rb for read only, wb+ for creating new file if not exists
+def rule_hash_write(mode:str, h:hashlib.sha256) -> bool:
+    with open("/etc/ryuhash/rules.hash", mode, encoding="utf-8") as f:
+        if f.read() == h.hexdigest().encode('utf-8'):
+            syslog.syslog(syslog.LOG_INFO, "Ryu Hash Check: No changes in the rules file")
+        else:
+            syslog.syslog(syslog.LOG_INFO, "Ryu Hash Check: Changes in the rules file, informing suricata...")
+            # Inform Suricata about the changes
+            for i in os.getenv("SURICATA-ADDRESSES").split(":"):
+                requests.get(f"http://{i}/fetch-rules", verify=False)
+            # Write the new hash to the file
+            with open("/etc/ryuhash/rules.hash", "w", encoding="utf-8") as f:
+                f.write(h.hexdigest())
+                f.flush()
 
 if __name__ == "__main__":
-
     # Load environment variables
     dotenv.load_dotenv()
     # Create a hash of the rules file
     h = hashlib.sha256()
     # Create hash file if not exists
-    Path("/etc/ryuhash/rules.hash").mkdir(parents=True, exist_ok=True)
+    Path(HASH_DIR).mkdir(parents=True, exist_ok=True)
 
     header = {"Authorization": os.getenv('MISP_API_TOKEN'),
               "Accept": "*/*",
@@ -25,20 +46,11 @@ if __name__ == "__main__":
     try:
         with requests.get(f"http://{os.getenv('MISP_API')}/events/nids/suricata/download", header = header, verify=False) as response:
             h.update(response.text.encode('utf-8')) 
-            print(h.hexdigest())
+            print(">>>", h.hexdigest())
+            if Path(HASH_LOC).exists():
+                rule_hash_write("rb")
+            else:
+                rule_hash_write("wb+")
 
-            # Compare the hash with the previous hash
-            with open("/etc/ryuhash/rules.hash", "rb", encoding="utf-8") as f:
-                if f.read() == h.hexdigest().encode('utf-8'):
-                    syslog.syslog(syslog.LOG_INFO, "Ryu Hash Check: No changes in the rules file")
-                else:
-                    syslog.syslog(syslog.LOG_INFO, "Ryu Hash Check: Changes in the rules file, informing suricata...")
-                    # Inform Suricata about the changes
-                    for i in os.getenv("SURICATA-ADDRESSES").split(":"):
-                        requests.get(f"http://{i}/fetch-rules", verify=False)
-                    # Write the new hash to the file
-                    with open("/etc/ryuhash/rules.hash", "w", encoding="utf-8") as f:
-                        f.write(h.hexdigest())
-                        f.flush()
     except Exception as e:
         syslog.syslog(syslog.LOG_ERR, f"Ryu Hash Check Error: {e}")
